663dc6e8c6d287ab972bb96ef57c5aec5a5bb229
Kurajsky, Peter <peter.kurajsky@sap.com>
2019-02-27 17:13:09 +0100

    "requiredBackendModules" as microfrontend visibility configuration

--- core/src/luigi-config/luigi-config.js
+++ core/src/luigi-config/luigi-config.js
@@ -244,10 +244,13 @@ function getUiEntities(entityname, environment, placements) {
                 order: node.order,
                 context: {
                   settings: node.settings
-                    ? { ...node.settings, ...(node.context || undefined) }
-                    : undefined
+                    ? { ...node.settings, ...(node.context || {}) }
+                    : {}
                 }
               };
+
+              n.context.requiredBackendModules =
+                node.requiredBackendModules || undefined;
 
               if (node.externalLink) {
                 delete n.viewUrl;
@@ -491,7 +494,7 @@ function getSelfSubjectRulesReview() {
   });
 }
 
-function navigationPermissionChecker(nodeToCheckPermissionsFor) {
+function checkRules(nodeToCheckPermissionsFor) {
   let hasPermissions = false;
   if (nodeToCheckPermissionsFor.adminOnly) {
     if (selfSubjectRulesReview.length > 0) {
@@ -509,6 +512,35 @@ function navigationPermissionChecker(nodeToCheckPermissionsFor) {
     hasPermissions = true;
   }
   return hasPermissions;
+}
+
+function checkRequiredBackendModules(nodeToCheckPermissionsFor) {
+  let hasPermissions = true;
+  if (
+    nodeToCheckPermissionsFor.context &&
+    nodeToCheckPermissionsFor.context.requiredBackendModules &&
+    nodeToCheckPermissionsFor.context.requiredBackendModules.length > 0
+  ) {
+    if (backendModules && backendModules.length > 0) {
+      nodeToCheckPermissionsFor.context.requiredBackendModules.forEach(
+        module => {
+          if (hasPermissions && backendModules.indexOf(module) === -1) {
+            hasPermissions = false;
+          }
+        }
+      );
+    } else {
+      hasPermissions = false;
+    }
+  }
+  return hasPermissions;
+}
+
+function navigationPermissionChecker(nodeToCheckPermissionsFor) {
+  return (
+    checkRules(nodeToCheckPermissionsFor) &&
+    checkRequiredBackendModules(nodeToCheckPermissionsFor)
+  );
 }
 
 function getBackendModules() {
